8f7b65f768fd6d1db38eb3f627c23d4c3d88ff4d
Samaksh Kaushik <Samaksh13kaushik@gmail.com>
2024-12-25 21:37:26 +0100

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,10 +1,8 @@
 import streamlit as st
-from Fractals import Mandelbrot, Julia, BurningShip,Tricorn
-
+from Fractals import Mandelbrot, Julia, BurningShip, Tricorn
 
 st.title("Fractals")
-st.write("This is simple visualisations of different fractals, it is not perfect and quite slow, but it hey it works.")
-
+st.write("This is a simple visualization of different fractals. It is not perfect and quite slow, but it works.")
 
 option = st.selectbox(
     'Which fractal would you like to see?',
@@ -13,17 +11,18 @@ option = st.selectbox(
     placeholder='Choose a fractal',
 )
 
-max_itter = int(st.number_input('Choose the number of iterations for fractal generation. Aim for a balance between detail and speed. Recommended range: 100-1000 iterations.', min_value=1, value=100, step=10      ))
+max_itter = int(st.number_input('Choose the number of iterations for fractal generation. Aim for a balance between detail and speed. Recommended range: 100-1000 iterations.',
+                               min_value=1, value=100, step=10))
 
-st.set_option('deprecation.showPyplotGlobalUse', False)
+# Remove the deprecated option setting
+# st.set_option('deprecation.showPyplotGlobalUse', False)
 
-
-
+# Create fractal based on the selection
 if option == 'Mandelbrot':
-    mandalbrot = Mandelbrot( xmin=-2, xmax=1, ymin=-1, ymax=1, width=1000, height=1000, max_iter= max_itter)
-    mandalbrot.display_fractal()
+    mandelbrot = Mandelbrot(xmin=-2, xmax=1, ymin=-1, ymax=1, width=1000, height=1000, max_iter=max_itter)
+    mandelbrot.display_fractal()
     st.pyplot()
-    
+
 elif option == 'Julia':
     julia_set = Julia(h_range=1200, w_range=1200, max_iter=max_itter)
     julia_set.draw_julia()
@@ -39,15 +38,19 @@ elif option == 'Tricorn':
     tricorn.display_fractal()
     st.pyplot()
 
+# Placeholder for future fractals
 elif option == 'Multibrot':
+    st.write("Multibrot fractal not implemented yet.")
     st.pyplot()
 
 elif option == 'Bifurcation':
+    st.write("Bifurcation fractal not implemented yet.")
     st.pyplot()
 
 elif option == 'Newton':
+    st.write("Newton fractal not implemented yet.")
     st.pyplot()
 
 elif option == 'Buddhabrot':
+    st.write("Buddhabrot fractal not implemented yet.")
     st.pyplot()
-
